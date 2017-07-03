196078686d2387b368032762ab12a6e7c558cdc8
David S. Miller <davem@davemloft.net>
2017-07-03 07:29:12 -0700

    BACKPORT: net: Remove all references to SKB_GSO_UDP.
    Such packets are no longer possible.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 880388aa3c07fdea4f9b85e35641753017b1852f)
    (cherry picked from commit 880388aa3c07)

--- net/sched/act_csum.c
+++ net/sched/act_csum.c
@@ -225,9 +225,6 @@ static int tcf_csum_ipv4_udp(struct sk_buff *skb,
 	const struct iphdr *iph;
 	u16 ul;
 
-	if (skb_is_gso(skb) && skb_shinfo(skb)->gso_type & SKB_GSO_UDP)
-		return 1;
-
 	/*
 	 * Support both UDP and UDPLITE checksum algorithms, Don't use
 	 * udph->len to get the real length without any protocol check,
@@ -280,9 +277,6 @@ static int tcf_csum_ipv6_udp(struct sk_buff *skb,
 	struct udphdr *udph;
 	const struct ipv6hdr *ip6h;
 	u16 ul;
-
-	if (skb_is_gso(skb) && skb_shinfo(skb)->gso_type & SKB_GSO_UDP)
-		return 1;
 
 	/*
 	 * Support both UDP and UDPLITE checksum algorithms, Don't use
