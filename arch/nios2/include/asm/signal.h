003c53c6970ac9eae2ac4db6a78e583315157207
Björn Töpel <bjorn.topel@intel.com>
2018-05-18 14:00:21 +0200

    BACKPORT: xsk: clean up SPDX headers
    Clean up SPDX-License-Identifier and removing licensing leftovers.
    
    Signed-off-by: Björn Töpel <bjorn.topel@intel.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    (cherry picked from commit dac09149d992995adbef0f472093fbb6940a8653)
    (cherry picked from commit dac09149d992)

--- arch/nios2/include/asm/signal.h
+++ arch/nios2/include/asm/signal.h
@@ -1,22 +1,14 @@
-/*
- * Copyright Altera Corporation (C) 2013. All rights reserved
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU General Public License,
- * version 2, as published by the Free Software Foundation.
- *
- * This program is distributed in the hope it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+/* SPDX-License-Identifier: GPL-2.0 */
+/* XDP user-space packet buffer
+ * Copyright(c) 2018 Intel Corporation.
  */
-#ifndef _NIOS2_SIGNAL_H
-#define _NIOS2_SIGNAL_H
 
-#include <uapi/asm/signal.h>
+#ifndef XDP_UMEM_PROPS_H_
+#define XDP_UMEM_PROPS_H_
 
-#endif	/* _NIOS2_SIGNAL_H */
+struct xdp_umem_props {
+	u32 frame_size;
+	u32 nframes;
+};
+
+#endif /* XDP_UMEM_PROPS_H_ */
