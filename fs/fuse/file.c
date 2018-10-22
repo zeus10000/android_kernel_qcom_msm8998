f6d9d5944ba57067402571e3a346d5e8ea3fde68
David Howells <dhowells@redhat.com>
2018-10-22 13:07:28 +0100

    BACKPORT: iov_iter: Use accessor function
    Use accessor functions to access an iterator's type and direction.  This
    allows for the possibility of using some other method of determining the
    type of iterator than if-chains with bitwise-AND conditions.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    
    (cherry picked from commit 00e23707442a75b404392cef1405ab4fd498de6b)
    (cherry picked from commit 00e23707442a)

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -1351,7 +1351,7 @@ static int fuse_get_user_pages(struct fuse_req *req, struct iov_iter *ii,
 	size_t nbytes = 0;  /* # bytes already packed in req */
 
 	/* Special case for kernel I/O: can copy directly into the buffer */
-	if (ii->type & ITER_KVEC) {
+	if (iov_iter_is_kvec(ii)) {
 		unsigned long user_addr = fuse_get_user_addr(ii);
 		size_t frag_size = fuse_get_frag_size(ii, *nbytesp);
 
