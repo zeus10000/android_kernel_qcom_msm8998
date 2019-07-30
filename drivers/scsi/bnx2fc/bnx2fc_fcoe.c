39119b970a37fa8b66c29c3bfe6f43a995376fda
Jonathan Lemon <jonathan.lemon@gmail.com>
2019-07-30 07:40:33 -0700

    BACKPORT: net: Use skb_frag_off accessors
    Use accessor functions for skb fragment's page_offset instead
    of direct references, in preparation for bvec conversion.
    
    Signed-off-by: Jonathan Lemon <jonathan.lemon@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit b54c9d5bd6e38edac9ce3a3f95f14a1292b5268d)
    (cherry picked from commit b54c9d5bd6e3)

--- drivers/scsi/bnx2fc/bnx2fc_fcoe.c
+++ drivers/scsi/bnx2fc/bnx2fc_fcoe.c
@@ -327,7 +327,7 @@ static int bnx2fc_xmit(struct fc_lport *lport, struct fc_frame *fp)
 			return -ENOMEM;
 		}
 		frag = &skb_shinfo(skb)->frags[skb_shinfo(skb)->nr_frags - 1];
-		cp = kmap_atomic(skb_frag_page(frag)) + frag->page_offset;
+		cp = kmap_atomic(skb_frag_page(frag)) + skb_frag_off(frag);
 	} else {
 		cp = (struct fcoe_crc_eof *)skb_put(skb, tlen);
 	}
