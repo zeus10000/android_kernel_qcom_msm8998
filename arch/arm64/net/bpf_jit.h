e098c81b2e55c3e71fbc06f6f5a6002fddbccf77
Luke Nelson <lukenels@cs.washington.edu>
2020-05-08 11:15:45 -0700

    BACKPORT: bpf, arm64: Optimize AND,OR,XOR,JSET BPF_K using arm64 logical immediates
    The current code for BPF_{AND,OR,XOR,JSET} BPF_K loads the immediate to
    a temporary register before use.
    
    This patch changes the code to avoid using a temporary register
    when the BPF immediate is encodable using an arm64 logical immediate
    instruction. If the encoding fails (due to the immediate not being
    encodable), it falls back to using a temporary register.
    
    Example of generated code for BPF_ALU32_IMM(BPF_AND, R0, 0x80000001):
    
    without optimization:
    
      24: mov  w10, #0x8000ffff
      28: movk w10, #0x1
      2c: and  w7, w7, w10
    
    with optimization:
    
      24: and  w7, w7, #0x80000001
    
    Since the encoding process is quite complex, the JIT reuses existing
    functionality in arch/arm64/kernel/insn.c for encoding logical immediates
    rather than duplicate it in the JIT.
    
    Co-developed-by: Xi Wang <xi.wang@gmail.com>
    Signed-off-by: Xi Wang <xi.wang@gmail.com>
    Signed-off-by: Luke Nelson <luke.r.nels@gmail.com>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>
    Link: https://lore.kernel.org/r/20200508181547.24783-3-luke.r.nels@gmail.com
    Signed-off-by: Will Deacon <will@kernel.org>
    (cherry picked from commit fd49591cb49b72abd1b665222a635ccb17df7923)
    (cherry picked from commit fd49591cb49b)

--- arch/arm64/net/bpf_jit.h
+++ arch/arm64/net/bpf_jit.h
@@ -189,12 +189,18 @@
 /* Rn & Rm; set condition flags */
 #define A64_TST(sf, Rn, Rm) A64_ANDS(sf, A64_ZR, Rn, Rm)
 
-/* HINTs */
-#define A64_HINT(x) aarch64_insn_gen_hint(x)
-
-/* BTI */
-#define A64_BTI_C  A64_HINT(AARCH64_INSN_HINT_BTIC)
-#define A64_BTI_J  A64_HINT(AARCH64_INSN_HINT_BTIJ)
-#define A64_BTI_JC A64_HINT(AARCH64_INSN_HINT_BTIJC)
+/* Logical (immediate) */
+#define A64_LOGIC_IMM(sf, Rd, Rn, imm, type) ({ \
+	u64 imm64 = (sf) ? (u64)imm : (u64)(u32)imm; \
+	aarch64_insn_gen_logical_immediate(AARCH64_INSN_LOGIC_##type, \
+		A64_VARIANT(sf), Rn, Rd, imm64); \
+})
+/* Rd = Rn OP imm */
+#define A64_AND_I(sf, Rd, Rn, imm) A64_LOGIC_IMM(sf, Rd, Rn, imm, AND)
+#define A64_ORR_I(sf, Rd, Rn, imm) A64_LOGIC_IMM(sf, Rd, Rn, imm, ORR)
+#define A64_EOR_I(sf, Rd, Rn, imm) A64_LOGIC_IMM(sf, Rd, Rn, imm, EOR)
+#define A64_ANDS_I(sf, Rd, Rn, imm) A64_LOGIC_IMM(sf, Rd, Rn, imm, AND_SETFLAGS)
+/* Rn & imm; set condition flags */
+#define A64_TST_I(sf, Rn, imm) A64_ANDS_I(sf, A64_ZR, Rn, imm)
 
 #endif /* _BPF_JIT_H */
