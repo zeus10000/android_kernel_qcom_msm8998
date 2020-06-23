33547ab0ab5be4c95ab14c865def64c7c2b69170
Yonghong Song <yhs@fb.com>
2020-06-23 16:08:11 -0700

    UPSTREAM: bpf: Add bpf_skc_to_{tcp, tcp_timewait, tcp_request}_sock() helpers
    Three more helpers are added to cast a sock_common pointer to
    an tcp_sock, tcp_timewait_sock or a tcp_request_sock for
    tracing programs.
    
    Signed-off-by: Yonghong Song <yhs@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Link: https://lore.kernel.org/bpf/20200623230811.3988277-1-yhs@fb.com
    (cherry picked from commit 478cfbdf5f13dfe09cfd0b1cbac821f5e27f6108)
    (cherry picked from commit 478cfbdf5f13)

--- scripts/bpf_helpers_doc.py
+++ scripts/bpf_helpers_doc.py
@@ -421,6 +421,9 @@ class PrinterHelpers(Printer):
             'struct tcphdr',
             'struct seq_file',
             'struct tcp6_sock',
+            'struct tcp_sock',
+            'struct tcp_timewait_sock',
+            'struct tcp_request_sock',
 
             'struct __sk_buff',
             'struct sk_msg_md',
@@ -459,6 +462,9 @@ class PrinterHelpers(Printer):
             'struct tcphdr',
             'struct seq_file',
             'struct tcp6_sock',
+            'struct tcp_sock',
+            'struct tcp_timewait_sock',
+            'struct tcp_request_sock',
     }
     mapped_types = {
             'u8': '__u8',
