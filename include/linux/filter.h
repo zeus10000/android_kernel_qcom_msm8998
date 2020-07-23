24f90a2c73bf140dd424944b816a3c87e3512e62
Christoph Hellwig <hch@lst.de>
2020-07-23 08:08:47 +0200

    UPSTREAM: net: switch copy_bpf_fprog_from_user to sockptr_t
    Pass a sockptr_t to prepare for set_fs-less handling of the kernel
    pointer from bpf-cgroup.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit b1ea9ff6aff2deae84eccaf0a07cd14912669680)
    (cherry picked from commit b1ea9ff6aff2)

--- include/linux/filter.h
+++ include/linux/filter.h
@@ -20,6 +20,7 @@
 #include <linux/kallsyms.h>
 #include <linux/if_vlan.h>
 #include <linux/vmalloc.h>
+#include <linux/sockptr.h>
 #include <crypto/sha.h>
 
 #include <net/sch_generic.h>
@@ -1276,6 +1277,6 @@ struct bpf_sockopt_kern {
 	s32		retval;
 };
 
-int copy_bpf_fprog_from_user(struct sock_fprog *dst, void __user *src, int len);
+int copy_bpf_fprog_from_user(struct sock_fprog *dst, sockptr_t src, int len);
 
 #endif /* __LINUX_FILTER_H__ */
